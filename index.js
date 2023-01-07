1167cb41d91689a481ad0484a30af370d0dce0c6
pablowinck <pablowinck123@gmail.com>
2023-01-07 10:36:02 -0300

    change json folder to tmp

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("./json/data.json");
+const router = jsonServer.router("./tmp/data.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
